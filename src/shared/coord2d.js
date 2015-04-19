83827c6054c146b28d0f96db9af7a7fde099194f
Alan Kendall <alankendall@gmail.com>
2015-04-18 23:36:21 -0500

    Basic Coord2d tests

--- src/shared/coord2d.js
+++ src/shared/coord2d.js
@@ -1,24 +1,28 @@
-(function(exports) {
+(function(module) {
 
     var RAD_TO_DEG = 57.2957795131;
 
-    /*
-     function Coord2d(x, y) {
-     if (typeof x === 'object') {
-     this.x = x.x;
-     this.y = x.y;
-     } else {
-     this.x = x || 0;
-     this.y = y || 0;
-     }
-     }
-     */
-
-    /*
-    function get() {
-
+    function Coord2d(x, y) {
+        x = x || 0;
+        y = y || 0;
+        this.x = x;
+        this.y = y;
     }
 
-    exports.get = get;
-    */
-})(typeof exports === 'undefined' ? this['Coord2d'] = {} : exports);
+    Coord2d.prototype = {
+        constructor: Coord2d,
+        setCartesian: function(x, y) {
+            this.x = x;
+            this.y = y;
+            return this;
+        }
+    }
+
+    Coord2d.fromXY = function(x, y) {
+        var c = new Coord2d();
+        return c.setCartesian(x, y);
+    };
+
+    module.Coord2d = Coord2d;
+
+})(typeof module === 'undefined' ? this : module);
--- src/shared/coord2d.spec.js
+++ src/shared/coord2d.spec.js
@@ -1,5 +1,29 @@
-describe('coord2d object and operations', function() {
-    it('defines coord2d', function() {
+describe('coord2d', function() {
+    it('is defined', function() {
         expect(Coord2d).not.toBeNull();
     });
+
+    it('is itself a constructor', function() {
+        expect(typeof new Coord2d()).toBe('object');
+    });
+
+    it('constructor with no args returns 0,0 point', function() {
+        var c = new Coord2d();
+        expect(c.x).toBe(0);
+        expect(c.y).toBe(0);
+    });
+
+    it('has factory method to create from x,y args', function() {
+        expect(Coord2d.fromXY).not.toBeNull();
+        var c = Coord2d.fromXY(3, 4);
+        expect(c.x).toBe(3);
+        expect(c.y).toBe(4);
+    });
+
+     it('has constructor to create from x,y args', function() {
+         var c = new Coord2d(3, 4);
+         expect(c.x).toBe(3);
+         expect(c.y).toBe(4);
+     });
+
 });
