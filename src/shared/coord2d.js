b869b4fb412138bf092afa523ed0315b4a714789
Alan Kendall <alankendall@gmail.com>
2015-04-18 23:54:00 -0500

    More tests

--- src/shared/coord2d.js
+++ src/shared/coord2d.js
@@ -3,6 +3,10 @@
     var RAD_TO_DEG = 57.2957795131;
 
     function Coord2d(x, y) {
+        if (isObjectArg(x)) {
+            y = x.y;
+            x = x.x;
+        }
         x = x || 0;
         y = y || 0;
         this.x = x;
@@ -15,13 +19,38 @@
             this.x = x;
             this.y = y;
             return this;
+        },
+        add: function(x, y) {
+            if (isObjectArg(x)) {
+                y = x.y;
+                x = x.x;
+            }
+            this.x += x;
+            this.y += y;
+            return this;
         }
     }
+
+    Coord2d.radiusFromXY = function(x, y) {
+        return Math.sqrt(x * x + y * y);
+    };
+
+    Coord2d.angleFromXY = function(x, y) {
+        return Math.atan2(y, x);
+    };
 
     Coord2d.fromXY = function(x, y) {
         var c = new Coord2d();
         return c.setCartesian(x, y);
     };
+
+    Coord2d.fromObject = function(o) {
+        return Coord2d.fromXY(o.x, o.y);
+    };
+
+    function isObjectArg(arg) {
+        return typeof arg === 'object';
+    }
 
     module.Coord2d = Coord2d;
 
--- src/shared/coord2d.spec.js
+++ src/shared/coord2d.spec.js
@@ -1,6 +1,6 @@
 describe('coord2d', function() {
     it('is defined', function() {
-        expect(Coord2d).not.toBeNull();
+        expect(Coord2d).toBeDefined();
     });
 
     it('is itself a constructor', function() {
@@ -14,7 +14,7 @@ describe('coord2d', function() {
     });
 
     it('has factory method to create from x,y args', function() {
-        expect(Coord2d.fromXY).not.toBeNull();
+        expect(Coord2d.fromXY).toBeDefined();
         var c = Coord2d.fromXY(3, 4);
         expect(c.x).toBe(3);
         expect(c.y).toBe(4);
@@ -26,4 +26,40 @@ describe('coord2d', function() {
          expect(c.y).toBe(4);
      });
 
+    it('has factory method to create from {x,y} object', function() {
+        expect(Coord2d.fromObject).toBeDefined();
+        var c = Coord2d.fromObject({x: 3, y: 4});
+        expect(c.x).toBe(3);
+        expect(c.y).toBe(4);
+    });
+
+    it('has constructor to create from {x,y} object', function() {
+        var c = new Coord2d({x: 3, y: 4});
+        expect(c.x).toBe(3);
+        expect(c.y).toBe(4);
+    });
+
+    it('has an add method to shift point from x,y args', function() {
+        var c = new Coord2d(2, 3);
+        c.add(1, -2);
+        expect(c.x).toBe(3);
+        expect(c.y).toBe(1);
+    });
+
+    it('has an add method to shift point from {x,y} object', function() {
+        var c = new Coord2d(2, 3);
+        c.add({x: 1, y: -2});
+        expect(c.x).toBe(3);
+        expect(c.y).toBe(1);
+    });
+
+    it('has static function for computing radius', function() {
+        expect(Coord2d.radiusFromXY).toBeDefined();
+        expect(Coord2d.radiusFromXY(3, 4)).toBe(5);
+    });
+
+    it('has static function for computing angle in rad from x,y args', function() {
+        expect(Coord2d.angleFromXY).toBeDefined();
+        expect(Coord2d.angleFromXY(3, 4)).toBeCloseTo(0.92729521, 7);
+    });
 });
