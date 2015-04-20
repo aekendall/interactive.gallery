3217ac76d65f32119cfa1b8fda42bc2f52dd86ee
Alan Kendall <alankendall@gmail.com>
2015-04-19 21:42:01 -0500

    updated the convention for test names

--- src/shared/coord2d.spec.js
+++ src/shared/coord2d.spec.js
@@ -3,7 +3,7 @@ describe('coord2d', function() {
         expect(Coord2d).toBeDefined();
     });
 
-    it('is itself a constructor', function() {
+    it('is a constructor', function() {
         expect(typeof new Coord2d()).toBe('object');
     });
 
@@ -13,47 +13,47 @@ describe('coord2d', function() {
         expect(c.y).toBe(0);
     });
 
-    it('has factory method to create from x,y args', function() {
+    it('library has factory to create from x,y args', function() {
         expect(Coord2d.fromXY).toBeDefined();
         var c = Coord2d.fromXY(3, 4);
         expect(c.x).toBe(3);
         expect(c.y).toBe(4);
     });
 
-     it('has constructor to create from x,y args', function() {
+     it('constructor can create from x,y args', function() {
          var c = new Coord2d(3, 4);
          expect(c.x).toBe(3);
          expect(c.y).toBe(4);
      });
 
-    it('has factory method to create from {x,y} object', function() {
+    it('library has factory to create from {x,y} object', function() {
         expect(Coord2d.fromObject).toBeDefined();
         var c = Coord2d.fromObject({x: 3, y: 4});
         expect(c.x).toBe(3);
         expect(c.y).toBe(4);
     });
 
-    it('has constructor to create from {x,y} object', function() {
+    it('constructor can create from {x,y} object', function() {
         var c = new Coord2d({x: 3, y: 4});
         expect(c.x).toBe(3);
         expect(c.y).toBe(4);
     });
 
-    it('has an add method to shift point from x,y args', function() {
+    it('class has an add method to shift point from x,y args', function() {
         var c = new Coord2d(2, 3);
         c.add(1, -2);
         expect(c.x).toBe(3);
         expect(c.y).toBe(1);
     });
 
-    it('has an add method to shift point from {x,y} object', function() {
+    it('class has an add method to shift point from {x,y} object', function() {
         var c = new Coord2d(2, 3);
         c.add({x: 1, y: -2});
         expect(c.x).toBe(3);
         expect(c.y).toBe(1);
     });
 
-    it('has functions for computing radius', function() {
+    it('library has functions for computing radius', function() {
         expect(Coord2d.radiusFromXY).toBeDefined();
         expect(Coord2d.radiusFromXY(3, 4)).toBe(5);
 
@@ -62,17 +62,17 @@ describe('coord2d', function() {
         expect(c.getRadius()).toBe(5);
     });
 
-    it('has static function for computing angle in rad from x,y args', function() {
+    it('library has function for computing angle in rad from x,y args', function() {
         expect(Coord2d.angleFromXY).toBeDefined();
         expect(Coord2d.angleFromXY(3, 4)).toBeCloseTo(0.92729521, 7);
     });
 
-    it('has static function to measure the distance between two x,y points', function() {
+    it('library has function to measure the distance between two x,y points', function() {
         expect(Coord2d.distBetween).toBeDefined();
         expect(Coord2d.distBetween(17, 21, 14, 17)).toBe(5);
     });
 
-    it('has method to normalize a coord to a radius of 1', function() {
+    it('class has method to normalize a coord to a radius of 1', function() {
         var c = new Coord2d(3, 4);
         expect(c.normalize).toBeDefined();
         c = c.normalize();
